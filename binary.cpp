44b7341bb4152c7137b85cc2b9f4c96f24f53b25
Timka111 <20080320tim@gmail.com>
2023-10-11 20:06:51 +0300

    feat: 4

---
+++ binary.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++)
+        a[i] = rand() % 10;
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+    int x; cin >> x;
+    cout << binary_search(a.begin(), a.end(), x) << endl;
+    cout << lower_bound(a.begin(), a.end(), x) - a.begin() << endl;
+    cout << upper_bound(a.begin(), a.end(), x) - a.begin() << endl;
+    return 0;
